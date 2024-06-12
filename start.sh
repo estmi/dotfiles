17769ae0b513b1978fb2ec8f91a8194c5a4abdf5
Esteve Miquel Guitart <estmiqgui@outlook.es>
2024-06-12 22:58:16 +0200

    script

--- start.sh
+++ start.sh
@@ -1,5 +1,10 @@
 #/bin/zsh
-rm ~/.zshrc 
+
+sudo apt install vim zsh
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+rm ~/.zshrc
 ln .zshrc ~/.zshrc
 rm ~/.ssh/config
 ln .ssh/config ~/.ssh/config
